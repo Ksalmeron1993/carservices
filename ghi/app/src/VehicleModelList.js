5530f70d39adea1de3d6c4433269867796ab7f31
Kevin Salmeron <ksalmeron1993@icloud.com>
2024-03-27 16:02:16 -0700

    fixed bug, changed variable name

--- ghi/app/src/VehicleModelList.js
+++ ghi/app/src/VehicleModelList.js
@@ -1,7 +1,7 @@
 import {useEffect, useState } from "react";
 
 const VehicleModelList = () => {
-    const [vehicleModels, setVehicleModels] = useState ([]);
+    const [models, setModels] = useState ([]);
 
     useEffect(() => {
         const fetchVehicleModels = async () => {
@@ -10,7 +10,7 @@ const VehicleModelList = () => {
 
             if(response.ok){
                 const data = await response.json();
-                setVehicleModels(data.vehicleModel);
+                setModels(data.models);
             }
         };
         fetchVehicleModels();
@@ -19,11 +19,11 @@ const VehicleModelList = () => {
     return (
     <div className="container mt-3">
         <div className="row row-cols-1 row-cols-md-2 g-4">
-            {vehicleModels && vehicleModels.map((vehicleModel) => (
-                    <div key={vehicleModel.id} className="col-lg-4 d-flex align-items-stretch">
+            {models.map((model) => (
+                    <div key={model.id} className="col-lg-4 d-flex align-items-stretch">
                         <div className="model-list card-body mb-3 shadow">
-                            <h5 className="card-header">{vehicleModel.manufacturer.name}</h5>
-                            <img src={vehicleModel.picture_url} className="card-img-top" alt={vehicleModel.name}/>
+                            <h5 className="card-header">{model.manufacturer.name}</h5>
+                            <img src={model.picture_url} className="card-img-top" alt={model.name}/>
                         </div>
                     </div>
             ))}
